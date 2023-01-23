a31af4158d3d328c6bb935c153a416fe8c8a9a24
Simon Lucas <simonlucas@hotmail.ca>
2023-01-22 21:55:29 -0500

    basic foundations

--- cardgame.js
+++ cardgame.js
@@ -1,17 +1,65 @@
-class Card {
-    constructor() {
+const SUITS = ["♥", "♦", "♠", "♣"]
+const VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
+class Card {
+    constructor(suit, value) {
+        this.suit = suit
+        this.value = value
     }
+
+    
 }
 
 class Deck {
-    constructor() {
+    constructor(cards = fullDeck()) {
+        this.cards = cards
 
     }
+
+    get numberOfCards() {
+        return this.cards.length
+    }
+
+    shuffle() {
+        for (let i = this.numberOfCards -1; i > 0; i--) {
+            const newIndex = Math.floor(Math.random() * (i + 1))
+            const oldIndex = this.cards[newIndex]
+            this.cards[newIndex] = this.cards[i]
+            this.cards[i] = oldIndex
+
+        }
+    }
+}
+
+function fullDeck () {
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value => {
+            return new Card(suit, value)
+        })
+    })
 }
 
 class Player {
-    constructor() {
+    constructor(PlayerName) {
+        this.PlayerName = PlayerName
+        this.CurrentHand = []
+        this.PlayerScore = 0
+        
+    }
+
+    gameSession() {
+        
+    }
+
+    namingSession() {
+
+    }
+
+    cardMatchup() {
         
     }
 }
+
+const deck = new Deck()
+deck.shuffle()
+console.log(deck.cards)
